ebe7a0ea632d5afc90d5bcbf0b56403b34ae0518
gayarabello <gayarabello@gmail.com>
2022-11-21 19:19:22 -0300

    Add new photos and categories

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,72 @@
 <script setup lang="ts">
 import ProductItem from "@/components/ProductItem.vue";
+import { ref, computed } from "vue";
+
+import {
+  BRINQUEDOS,
+  ELETRODOMESTICOS,
+  AUDIOVISUAL,
+  ANTIGUIDADES,
+  COZINHA,
+  DECORACAO,
+  MOVEIS,
+  COSTURA,
+} from "@/data/filters.js";
 
 const items: number[] = [];
-for (let i = 1; i < 163; i++) {
-  items.push(i);
-}
+const showNew = ref(true);
+
+const itemsCount = computed(() => {
+  const count = showNew.value ? 342 : 163;
+  let items = [];
+  for (let i = 1; i < count; i++) {
+    items.push(i);
+  }
+  return items;
+});
 </script>
 
 <template>
-  <div style="display: flex; flex-wrap: wrap; justify-content: space-between;">
-    <ProductItem v-for="item in items" :key="item" :id="item" />
+  <button @click="showNew = !showNew">
+    {{ showNew ? "ver antigos" : "ver novos" }}
+  </button>
+  <div v-if="showNew">
+    <div style="display: flex; flex-wrap: wrap; justify-content: space-between">
+      <ProductItem
+        v-for="item in itemsCount"
+        :key="item"
+        :id="item"
+        new-images
+      />
+    </div>
+  </div>
+  <div v-else>
+    <div style="display: flex; flex-wrap: wrap; justify-content: space-between">
+      <ProductItem v-for="item in itemsCount" :key="item" :id="item" />
+    </div>
+    <div style="display: flex; flex-wrap: wrap; justify-content: space-between">
+      <ProductItem v-for="item in BRINQUEDOS" :key="item" :id="item" />
+    </div>
+    <div style="display: flex; flex-wrap: wrap; justify-content: space-between">
+      <ProductItem v-for="item in ELETRODOMESTICOS" :key="item" :id="item" />
+    </div>
+    <div style="display: flex; flex-wrap: wrap; justify-content: space-between">
+      <ProductItem v-for="item in AUDIOVISUAL" :key="item" :id="item" />
+    </div>
+    <div style="display: flex; flex-wrap: wrap; justify-content: space-between">
+      <ProductItem v-for="item in ANTIGUIDADES" :key="item" :id="item" />
+    </div>
+    <div style="display: flex; flex-wrap: wrap; justify-content: space-between">
+      <ProductItem v-for="item in COZINHA" :key="item" :id="item" />
+    </div>
+    <div style="display: flex; flex-wrap: wrap; justify-content: space-between">
+      <ProductItem v-for="item in DECORACAO" :key="item" :id="item" />
+    </div>
+    <div style="display: flex; flex-wrap: wrap; justify-content: space-between">
+      <ProductItem v-for="item in MOVEIS" :key="item" :id="item" />
+    </div>
+    <div style="display: flex; flex-wrap: wrap; justify-content: space-between">
+      <ProductItem v-for="item in COSTURA" :key="item" :id="item" />
+    </div>
   </div>
 </template>
